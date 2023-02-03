4e1a5ee782e4d576fdf0d68824f3ad9fda6943ee
eaccus <mauro@eaccus.com>
2023-02-02 22:47:09 -0800

    as

--- dist/ThirdPerson.sh
+++ dist/ThirdPerson.sh
@@ -3,4 +3,4 @@ UE_TRUE_SCRIPT_NAME=$(echo \"$0\" | xargs readlink -f)
 UE_PROJECT_ROOT=$(dirname "$UE_TRUE_SCRIPT_NAME")
 chmod +x "$UE_PROJECT_ROOT/Binaries/Linux/ThirdPerson"
 #"$UE_PROJECT_ROOT/Binaries/Linux/ThirdPerson" ThirdPerson "$@" 
-"$UE_PROJECT_ROOT/Binaries/Linux/ThirdPerson" "-RenderOffscreen" "-Windowed" "-ForceRes" "-ResX=1920" "-ResY=1080" "-PixelStreamingIP=127.0.0.1" "-PixelStreamingPort=8888"
+"$UE_PROJECT_ROOT/Binaries/Linux/ThirdPerson" ThirdPerson "-RenderOffscreen" "-Windowed" "-ForceRes" "-ResX=1920" "-ResY=1080" "-PixelStreamingIP=127.0.0.1" "-PixelStreamingPort=8888"
