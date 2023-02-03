7556eddb12a4cac78071489ad6defeabee4e05db
eaccus <mauro@eaccus.com>
2023-02-02 22:18:45 -0800

    asd

--- dist/ThirdPerson.sh
+++ dist/ThirdPerson.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 UE_TRUE_SCRIPT_NAME=$(echo \"$0\" | xargs readlink -f)
 UE_PROJECT_ROOT=$(dirname "$UE_TRUE_SCRIPT_NAME")
+echo "#######################PWD####################"
 pwd
+echo "#######################PWD####################"
 ls -lt "$UE_PROJECT_ROOT"
 chmod +x "$UE_PROJECT_ROOT/ThirdPerson/Binaries/Linux/ThirdPerson"
 "$UE_PROJECT_ROOT/ThirdPerson/Binaries/Linux/ThirdPerson" ThirdPerson "$@" 
