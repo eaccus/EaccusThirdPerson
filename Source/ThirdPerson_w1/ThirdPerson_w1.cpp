a32ca984ae81342984c55f00014b86e04b309c3a
eaccus <mauro@eaccus.com>
2023-02-04 11:51:49 -0800

    clean

--- Source/ThirdPerson_w1/ThirdPerson_w1.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ThirdPerson_w1.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ThirdPerson_w1, "ThirdPerson_w1" );
- 
